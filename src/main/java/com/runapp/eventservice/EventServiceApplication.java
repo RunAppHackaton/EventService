f647b7fe07cf36c4c31c308b89eeb2d088e9ce32
denpool <den.dyrbalov25@gmail.com>
2023-12-25 20:39:09 +0100

    update config env

--- src/main/java/com/runapp/eventservice/EventServiceApplication.java
+++ src/main/java/com/runapp/eventservice/EventServiceApplication.java
@@ -1,5 +1,6 @@
 package com.runapp.eventservice;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,7 +10,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableDiscoveryClient
 public class EventServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EventServiceApplication.class, args);
 	}
